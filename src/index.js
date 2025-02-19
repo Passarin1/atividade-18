b3d1c780ef444d42e29d5fd0cde6fa6d4964e8b8
Luís Henrique <165834056+Passarin1@users.noreply.github.com>
2025-02-19 23:53:05 +0000

    deprecao

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());                      // Middleware para parsing JSON
 
 // Rota raiz
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   console.log("Rota GET/ solicitada");
   res.json({
     nome: "Luis Henrique",                   // Substitua pelo seu nome
