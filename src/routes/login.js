19a5fd0f8694dd956394162edb049bb6389c22c2
Luís Henrique <165834056+Passarin1@users.noreply.github.com>
2025-03-11 18:41:51 +0000

    Add - Nome

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,6 +4,15 @@ import { autenticarUsuario } from "../db/index.js";
 
 const router = Router();
 
+router.get("/auth", verificarAutenticacao, async (req, res) => {
+  console.log("Rota GET /auth solicitada");
+  try {
+    res.status(200).json({ user: `${req.userId}` });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
 router.post("/login", async (req, res) => {
   console.log("Rota POST /login solicitada");
   try {
